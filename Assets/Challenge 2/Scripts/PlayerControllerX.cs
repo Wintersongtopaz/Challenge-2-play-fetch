04ea47c6a83fa4128725b6d57dcc439021861a46
Haleysongtopaz <Haleysongtopaz@gmail.com>
2024-02-22 18:16:32 -0500

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public GameObject spawnedDog;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(spawnedDog);
+            spawnedDog = Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
